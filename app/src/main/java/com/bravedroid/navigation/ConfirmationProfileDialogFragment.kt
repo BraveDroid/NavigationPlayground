f33f74938ea61db5534ec6a7c113131a095144c2
houssemzaier <houssem.zaier@gmail.com>
2019-12-29 23:28:11 +0100

    [nav-2] setup navigation using Directions andnavigation to dialogs (fullscreen and not fullscreen)

---
+++ app/src/main/java/com/bravedroid/navigation/ConfirmationProfileDialogFragment.kt
@@ -0,0 +1,39 @@
+package com.bravedroid.navigation
+
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+
+
+class ConfirmationProfileDialogFragment : DialogFragment() {
+    companion object {
+        const val TAG = "confirmationProfileDialogFragment"
+    }
+
+    /** The system calls this to get the DialogFragment's layout, regardless
+    of whether it's being displayed as a dialog or an embedded fragment. */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmation_profile_dialog, container, false)
+    }
+
+    /** The system calls this only when creating the layout in a dialog. */
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        return dialog
+    }
+}
--- app/src/main/java/com/bravedroid/navigation/MainFragment.kt
+++ app/src/main/java/com/bravedroid/navigation/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_main.*
 
@@ -19,14 +20,24 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val callback: (View) -> Unit = {
-            when (it.id) {
-                R.id.view_transactions_btn -> findNavController().navigate(R.id.action_mainFragment_to_viewTransactionFragment4)
-                R.id.send_money_btn -> findNavController().navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
-                R.id.view_balance_btn -> findNavController().navigate(R.id.action_mainFragment_to_viewBalanceFragment2)
-                R.id.profile_btn -> findNavController().navigate(R.id.profileFragment)
-                R.id.about_us_btn -> findNavController().navigate(R.id.aboutActivity)
+    override fun onViewCreated(inflatedview: View, savedInstanceState: Bundle?) {
+
+        val callback: (View) -> Unit = { view ->
+            when (view.id) {
+                //navigate using action Id (findNavController is the extension of the activity )
+                R.id.view_transactions_btn -> requireActivity().findNavController(view.id).navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+                //navigate using action Id (findNavController is the extension of the view)
+                R.id.send_money_btn -> view.findNavController().navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+                //navigate using action Id (findNavController is the extension of the fragment)
+                R.id.view_balance_btn -> findNavController().navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+                //navigate using destination Id
+                R.id.profile_btn -> requireActivity().findNavController(view.id).navigate(R.id.profileFragment)
+                R.id.about_us_btn -> requireActivity().findNavController(view.id).navigate(R.id.aboutActivity)
+                //navigate using navDirections from safe args
+                R.id.settings_btn -> {
+                    val action = MainFragmentDirections.actionMainFragmentToChooseRecipientFragment()
+                   findNavController().navigate(action)
+                }
             }
         }
 
@@ -35,5 +46,6 @@ class MainFragment : Fragment() {
         view_transactions_btn.setOnClickListener(callback)
         profile_btn.setOnClickListener(callback)
         about_us_btn.setOnClickListener(callback)
+        settings_btn.setOnClickListener(callback)
     }
 }
