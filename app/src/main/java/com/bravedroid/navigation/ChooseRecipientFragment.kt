b5e50b028650a7762ceb588df8d4e6dabff8a2dd
houssemzaier <houssem.zaier@gmail.com>
2020-01-08 16:09:28 +0100

    [nav-23] add all types of supported animation with navigation component lib

--- app/src/main/java/com/bravedroid/navigation/ChooseRecipientFragment.kt
+++ app/src/main/java/com/bravedroid/navigation/ChooseRecipientFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bravedroid.navigation.util.navigateWithAnimation
 import kotlinx.android.synthetic.main.fragment_choose_recipient.*
 
 class ChooseRecipientFragment : Fragment() {
@@ -24,14 +25,14 @@ class ChooseRecipientFragment : Fragment() {
 
         next_btn.setOnClickListener {
             val name = input_recipient.text.toString()
-            findNavController().navigate(
+            findNavController().navigateWithAnimation(
                 ChooseRecipientFragmentDirections.actionChooseRecipientFragmentToSpecifyAmountFragment(
                     name
                 )
             )
         }
         info_btn.setOnClickListener {
-            findNavController().navigate(ChooseRecipientFragmentDirections.actionGlobalInfoFragment())
+            findNavController().navigateWithAnimation(ChooseRecipientFragmentDirections.actionGlobalInfoFragment())
         }
 
         cancel_btn.setOnClickListener {
